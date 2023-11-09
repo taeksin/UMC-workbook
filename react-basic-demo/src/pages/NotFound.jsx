74d52c9cd3d328999f6cf1849a0211dd7dd305f2
taeksin <yoyo2521@naver.com>
2023-11-09 19:01:31 +0900

    finally week6

--- react-basic-demo/src/pages/NotFound.jsx
+++ react-basic-demo/src/pages/NotFound.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const goToHomePage = () => {
+    navigate('/'); // 홈 화면으로 이동
+  };
+
   return (
     <>
-    <div> NotFound </div>
-    <p > 메인페이지로가기 </p>
+      <div>NotFound</div>
+      <p onClick={goToHomePage} style={{ cursor: 'pointer' }}>
+        메인 페이지로 가기
+      </p>
     </>
-    
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
